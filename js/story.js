584e8003fc6a4be55ba148e4d1b66dd06f72d119
redspacecat <162653322+redspacecat@users.noreply.github.com>
2024-06-28 10:38:19 -0700

    title & text

--- js/story.js
+++ js/story.js
@@ -1,12 +1,33 @@
+let page
+let story
+
 window.onload = start()
 
+window.onerror = function errorHandler(errorMsg, url, lineNumber) {
+    alert("Error occured: " + errorMsg);
+    return false;
+}
+
+function loadPage(num) {
+    try {
+        let pageData = story.story.num
+        storyDiv = document.getElementById("story-stuff")
+        storyDiv.innerHTML += `<p>${pageData.title}<p><br><p>${pageData.text}</p>`
+    }
+    catch {
+        alert("Error occured loading page " + num)
+    }
+}
 
 function start() {
-    let storyData = localStorage.getItem("story")
-    if (storyData == null || undefined) {
+    story = localStorage.getItem("story")
+    if (story == null || undefined) {
         window.location.pathname = window.location.pathname.replace("story", "")
     } else {
-        storyData = JSON.parse(storyData)
-        alert("Welcome to " + storyData.info.title)
+
+        story = JSON.parse(story)
+        page = parseInt(window.location.hash.slice(1, ))
+        loadPage(1)
+
     }
 }
