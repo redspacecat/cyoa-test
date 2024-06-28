fe37d6c67776d6509620af6c7bf7c46aeafa1e9b
redspacecat <162653322+redspacecat@users.noreply.github.com>
2024-06-28 07:55:04 -0700

    file upload

---
+++ js/main.js
@@ -0,0 +1,29 @@
+document.getElementById('input-file')
+  .addEventListener('change', getFile)
+
+function getFile(event) {
+	const input = event.target
+  if ('files' in input && input.files.length > 0) {
+	  placeFileContent(
+      input.files[0])
+  }
+}
+
+function finish(result) {
+	alert(result)
+}
+
+function placeFileContent(file) {
+	readFileContent(file).then(content => {
+    finish(content)
+  }).catch(error => console.log(error))
+}
+
+function readFileContent(file) {
+	const reader = new FileReader()
+  return new Promise((resolve, reject) => {
+    reader.onload = event => resolve(event.target.result)
+    reader.onerror = error => reject(error)
+    reader.readAsText(file)
+  })
+}
