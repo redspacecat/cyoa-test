b5d6a0553e3dc5a1e749ede7fa01272564a34774
redspacecat <162653322+redspacecat@users.noreply.github.com>
2024-06-30 08:28:57 -0700

    changes

--- js/create.js
+++ js/create.js
@@ -1,5 +1,21 @@
+function download(filename, text) {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:application/octet-stream;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+  
+    element.style.display = 'none';
+    document.body.appendChild(element);
+  
+    element.click();
+  
+    document.body.removeChild(element);
+}
+  
+// Start file download.
+// download("hello.json","This is the content of my file :)");
+
 window.onload = begin
 
 function begin() {
-    
+
 }
